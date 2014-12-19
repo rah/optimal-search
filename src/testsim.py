4f300ca058f3a85f479889ebf01010ea15460add
rah <rossh00@gmail.com>
2014-12-19 15:39:27 +1000

    generic test

--- src/testsim.py
+++ src/testsim.py
@@ -1,13 +1,14 @@
 #! /usr/env/python
 import matplotlib.pyplot as plt
 from animate import Animate
+from searcher import Searcher
 
 
-def runsim():
+def runsim(clazz=Animate, *args):
     '''
     run a simple simulation of movement
     '''
-    mover = Animate()
+    mover = clazz(*args)
 
     for i in range(100):
         mover.move()
@@ -15,3 +16,7 @@ def runsim():
 
     x, y = mover.get_movement()
     plt.plot(x, y)
+
+
+runsim(Animate, 90.0, 1.0, 180.0, 0.75, 0.0, 0.0)
+runsim(Searcher, 90.0, 1.0, 180.0, 0.75, 0.0, 0.0, 0)
