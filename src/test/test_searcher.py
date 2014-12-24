c48974d8c790711fc388c853c443c5e17aa87077
rah <rah>
2014-12-24 15:47:26 +1000

    initial test file

---
+++ src/test/test_searcher.py
@@ -0,0 +1,25 @@
+import unittest
+import searcher
+
+
+class TestSearcher(unittest.TestCase):
+
+    def setUp(self):
+        self.mover = searcher.Searcher()
+
+    def test_instance(self):
+        self.assertIsNotNone(self.mover)
+        self.assertTrue(self.mover.direction == 0.0)
+        self.assertTrue(self.mover.max_speed == 1.0)
+        self.assertTrue(self.mover.max_turn == 30.0)
+        self.assertTrue(self.mover.positive_turn == 0.5)
+        self.assertTrue(self.mover.curr_x == 0.0)
+        self.assertTrue(self.mover.curr_y == 0.0)
+
+        self.assertIsNotNone(self.mover.X)
+        self.assertIsNotNone(self.mover.Y)
+
+        self.assertTrue(self.mover.giving_up_time == 0)
+        self.assertTrue(
+            self.mover.time_since_encounter ==
+            searcher.Searcher.MAX_TIME_SINCE_ENC)
