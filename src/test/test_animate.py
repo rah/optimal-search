0066b9708e3bc2664b9c7a9d3a445ee2999c717b
rah <rah>
2014-12-24 14:18:01 +1000

    Nose tests

--- src/test/test_animate.py
+++ src/test/test_animate.py
@@ -7,9 +7,11 @@ class TestAnimate(unittest.TestCase):
     def setUp(self):
         self.mover = animate.Animate()
 
-    def test_move(self):
+    def test_instance(self):
         self.assertIsNotNone(self.mover)
+        self.assertTrue(self.mover.direction == 0)
 
+    def test_move(self):
         x, y = self.mover.get_movement()
         xlen1 = len(x)
         self.assertTrue(xlen1 > 0)
@@ -23,7 +25,16 @@ class TestAnimate(unittest.TestCase):
         self.assertTrue(x[xlen1-1] != x[xlen2-1])
 
     def test_rel_move(self):
-        pass
+        speed = 1.0
+        direction = 0.0
+
+        x, y = self.mover.relMove(direction, speed)
+        self.assertEqual(x, 1.0)
+        self.assertEqual(y, 0.0)
 
     def test_set_direction(self):
-        pass
+        self.mover.direction = 0.0
+        self.assertEqual(self.mover.direction, 0.0)
+
+        self.mover.set_direction(30.0)
+        self.assertEqual(self.mover.direction, 30.0)
