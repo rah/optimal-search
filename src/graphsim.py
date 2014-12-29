c9917bfeb0b3ed53668225d1d4fa89fcd7b7efdc
rah <rah>
2014-12-29 13:33:10 +1000

    integrate searcher into graphsim

--- src/graphsim.py
+++ src/graphsim.py
@@ -1,9 +1,12 @@
 #! /usr/env/python
 from random import randint
+from random import random
 from environment import Environment
+from searcher import Searcher
 from graphics import GraphWin
 from graphics import Point
 from graphics import Circle
+from graphics import Line
 
 
 win = GraphWin("test", 800, 800)
@@ -16,10 +19,22 @@ env = Environment(1000, 1000, 10)
 env.create_patches()
 for patch in env.patches:
     c = Circle(Point(patch.x_pos, patch.y_pos), patch.radius)
+    c.setOutline("red")
     c.draw(win)
 
     # Add the entities
     patch.create_entities(randint(0, 10))
     for entity in patch.entities:
         p = Point(entity.x_pos, entity.y_pos)
+        p.setOutline("blue")
         p.draw(win)
+
+x = random() * env.length
+y = random() * env.width
+s = Searcher(x_pos=x, y_pos=y)
+
+for i in range(10000):
+    s.move()
+    p = Line(Point(s.X[i], s.Y[i]), Point(s.X[i+1], s.Y[i+1]))
+    p.setOutline("green")
+    p.draw(win)
