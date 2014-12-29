e4ff4cd972ac8587dd54d78bd4ebf14c668f2b56
rah <rah>
2014-12-29 17:01:57 +1000

    add searcher to sim

--- src/graphsim.py
+++ src/graphsim.py
@@ -8,6 +8,24 @@ from graphics import Point
 from graphics import Circle
 from graphics import Line
 
+MAX_MOVES = 50000
+
+
+def check_and_set_bounds(env, searcher):
+    if searcher.curr_x <= 0.0:
+        searcher.setx(env.length - 1.0)
+        return True
+    if searcher.curr_x >= env.length:
+        searcher.setx(0.0 + 1.0)
+        return True
+    if searcher.curr_y <= 0.0:
+        searcher.sety(env.width - 1.0)
+        return True
+    if searcher.curr_y >= env.width:
+        searcher.sety(0.0 + 1.0)
+        return True
+
+    return False
 
 win = GraphWin("test", 800, 800)
 win.setCoords(0, 0, 1000, 1000)
@@ -33,8 +51,18 @@ x = random() * env.length
 y = random() * env.width
 s = Searcher(x_pos=x, y_pos=y)
 
-for i in range(10000):
+i = 0
+while (i < MAX_MOVES):
     s.move()
-    p = Line(Point(s.X[i], s.Y[i]), Point(s.X[i+1], s.Y[i+1]))
-    p.setOutline("green")
-    p.draw(win)
+
+    if not check_and_set_bounds(env, s):
+        p = Line(Point(s.X[i], s.Y[i]), Point(s.X[i+1], s.Y[i+1]))
+        p.setOutline("green")
+        p.draw(win)
+
+    i += 1
+    if win.checkMouse() is not None:
+        break
+
+if win.getKey() == "k":
+    win.close()
