fe4c8c884afbd30921994814b76786d155e1fa56
rah <rah>
2014-12-29 12:25:19 +1000

    add patches and entities and display

--- src/graphsim.py
+++ src/graphsim.py
@@ -1,23 +1,25 @@
 #! /usr/env/python
-import sys
-
+from random import randint
 from environment import Environment
-
-sys.path.append("c:/Users/Ross/workspace/graphics")
 from graphics import GraphWin
 from graphics import Point
 from graphics import Circle
 
 
-win = GraphWin("test", 800, 600)
+win = GraphWin("test", 800, 800)
 win.setCoords(0, 0, 1000, 1000)
 
+# Set up the environment
 env = Environment(1000, 1000, 10)
+
+# create patches
 env.create_patches()
-
-patches = env.patches
-
-for patch in patches:
-    # print patch.x_pos, patch.y_pos, patch.radius
+for patch in env.patches:
     c = Circle(Point(patch.x_pos, patch.y_pos), patch.radius)
     c.draw(win)
+
+    # Add the entities
+    patch.create_entities(randint(0, 10))
+    for entity in patch.entities:
+        p = Point(entity.x_pos, entity.y_pos)
+        p.draw(win)
