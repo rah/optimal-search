509b01b3fc4039fff94f958acac43ab6921d6023
rah <rah>
2014-12-29 17:01:27 +1000

    setx and sety on entity

--- src/entity.py
+++ src/entity.py
@@ -10,3 +10,11 @@ class Entity(object):
             y_pos=0.0):
         self.x_pos = x_pos
         self.y_pos = y_pos
+
+    def setx(self, x):
+        self.curr_x = x
+        self.X[len(self.X) - 1] = x
+
+    def sety(self, y):
+        self.curr_y = y
+        self.Y[len(self.Y) - 1] = y
