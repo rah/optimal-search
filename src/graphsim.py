8d9fc42a83134e643690d0adf4efafa2eb890a2c
rah <rah>
2014-12-31 15:49:10 +1000

    refactor

--- src/graphsim.py
+++ src/graphsim.py
@@ -11,58 +11,64 @@ from graphics import Line
 MAX_MOVES = 50000
 
 
-def check_and_set_bounds(env, searcher):
-    if searcher.curr_x <= 0.0:
-        searcher.setx(env.length - 1.0)
-        return True
-    if searcher.curr_x >= env.length:
-        searcher.setx(0.0 + 1.0)
-        return True
-    if searcher.curr_y <= 0.0:
-        searcher.sety(env.width - 1.0)
-        return True
-    if searcher.curr_y >= env.width:
-        searcher.sety(0.0 + 1.0)
-        return True
+def draw_patches(env, win):
+    for patch in env.children:
+        # Draw the patch
+        c = Circle(Point(patch.x_pos, patch.y_pos), patch.radius)
+        c.setOutline("red")
+        c.draw(win)
 
-    return False
+        # Draw the entities in the patch
+        for entity in patch.children:
+            p = Point(entity.x_pos, entity.y_pos)
+            p.setOutline("blue")
+            p.draw(win)
+
+
+def draw_searcher_move(s, i, win):
+        p = Line(Point(s.X[i], s.Y[i]), Point(s.X[i+1], s.Y[i+1]))
+        p.setOutline("green")
+        p.draw(win)
+
+
+def remove_entity(entity):
+    pass
+
 
 win = GraphWin("test", 800, 800)
 win.setCoords(0, 0, 1000, 1000)
 
 # Set up the environment
 env = Environment(1000, 1000, 10)
-
-# create patches
-env.create_patches()
-for patch in env.patches:
-    c = Circle(Point(patch.x_pos, patch.y_pos), patch.radius)
-    c.setOutline("red")
-    c.draw(win)
-
-    # Add the entities
-    patch.create_entities(randint(0, 10))
-    for entity in patch.entities:
-        p = Point(entity.x_pos, entity.y_pos)
-        p.setOutline("blue")
-        p.draw(win)
+for patch in env.children:
+    patch.create_entities(10)
+    
+draw_patches(env, win)
 
 x = random() * env.length
 y = random() * env.width
-s = Searcher(x_pos=x, y_pos=y)
+s = Searcher(x_pos=x, y_pos=y, parent=env)
 
 i = 0
 while (i < MAX_MOVES):
     s.move()
+    draw_searcher_move(s, i, win)
 
-    if not check_and_set_bounds(env, s):
-        p = Line(Point(s.X[i], s.Y[i]), Point(s.X[i+1], s.Y[i+1]))
-        p.setOutline("green")
-        p.draw(win)
+    entity = s.capture()
+    if entity is not None:
+        remove_entity(entity)
 
     i += 1
-    if win.checkMouse() is not None:
+
+    # Check interaction
+    key = win.checkKey()
+    if key == "p":
+        if win.getKey() == "s":
+            break
+        else:
+            continue
+    elif key == "s":
         break
 
-if win.getKey() == "k":
+if win.getKey() == "c":
     win.close()
