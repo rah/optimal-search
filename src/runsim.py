4e2b15622dab986962e83fedd764b655ed622fa9
rah <rah>
2015-02-02 17:13:38 +1000

    run with parameters

--- src/runsim.py
+++ src/runsim.py
@@ -1,40 +1,90 @@
 #! /usr/env/python
+"""
+Provides a convenient wrapper to run a set of simulation.
+Parameters for the simulations are contained in a properties file.
+"""
 from environment import Environment
 from searcher import Searcher
-import graphsim as gs
 
-MAX_MOVES = 50000
+import random
+from scipy import stats
+import numpy as np
+import pylab
+
+entity_results = []
+captured_results = []
 
 
-def remove_entity(entity):
-    patch = entity.parent
-    patch.children.remove(entity)
+def default_params():
+    params = {
+        'env_size': 1000,
+        'n_patches': 20,
+        'n_trials': 100,
+        'max_moves': 5000,
+        'max_entities_per_patch': 50,
+        'min_entities_per_patch': 5,
+    }
+    return params
 
 
-# Set up the environment
-env = Environment(1000, 1000, 20)
-for patch in env.children:
-    patch.create_entities(10)
+def config_params(config_file):
+    return {}
 
-s = Searcher(x_pos=(env.length / 2.0),
-             y_pos=(env.width / 2.0),
-             parent=env)
 
-win = gs.setup_graphics_window("test", 800, 800, 1000, 1000)
-gs.draw_environment(env, win)
+def runsim(config_file=None):
+    p = {}
 
-for i in range(MAX_MOVES):
-    s.move()
+    # get the paramters for the simulation
+    if config_file is None:
+        p = default_params()
+    else:
+        p = config_params(config_file)
 
-    gs.draw_searcher_move(s, i, win)
+    for trail in range(p['n_trials']):
+        # Set up the environment
+        env = Environment(p['env_size'], p['env_size'], p['n_patches'])
+        entities = random.randint(p['min_entities_per_patch'],
+                                  p['max_entities_per_patch'])
+        for patch in env.children:
+            patch.create_entities(entities)
 
-    entity = s.detect()
-    captured = s.capture(entity)
-    if captured is not None:
-        gs.undraw_entity(captured, win)
-        captured.remove_self()
+        s = Searcher(x_pos=(env.length / 2.0),
+                     y_pos=(env.width / 2.0),
+                     parent=env)
 
-    if gs.breakout(win):
-        break
+        for i in range(p['max_moves']):
+            s.move()
+            entity = s.detect()
+            s.capture(entity)
 
-gs.closewin(win)
+        entity_results.append(entities)
+        captured_results.append(len(s.captured))
+
+        # for k in range(len(entity_results)):
+        #     print(entity_results[k], captured_results[k])
+
+    x = np.array(entity_results)
+    y = np.array(captured_results)
+
+    slope, intercept, r_value, p_value, \
+        slope_std_error = stats.linregress(x, y)
+
+    print "Slope, intercept:", slope, intercept
+    print "R-squared:", r_value**2
+
+    # Calculate some additional outputs
+    predict_y = intercept + slope * x
+    pred_error = y - predict_y
+    degrees_of_freedom = len(x) - 2
+    residual_std_error = np.sqrt(np.sum(pred_error**2) / degrees_of_freedom)
+    print "Residual Std Error = ", residual_std_error
+
+    # Plotting
+    pylab.plot(x, y, 'o')
+    pylab.plot(x, predict_y, 'k-')
+    pylab.show()
+
+
+
+
+
