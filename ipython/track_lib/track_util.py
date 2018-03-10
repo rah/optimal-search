a9c8c250c2b12d4539cc0e96e90ab69150ec05c5
rah <rossh00@gmail.com>
2018-03-10 12:02:49 +1000

    updated tracks to use track util

--- ipython/track_lib/track_util.py
+++ ipython/track_lib/track_util.py
@@ -40,15 +40,39 @@ def plot_tracks(df):
     ''' Plot snail tracks showing track, distance moved, 
         and abs angle for each time point 
     '''
-    dist_ylim = (0, 1.2)
+    dist_ylim = (0, max_distance(df))
     angle_ylim = (0, 180)
 
-    fig, axes = plt.subplots(nrows=len(df.keys()), ncols=3, figsize=(20, 45))
+    plot_depth = 5
+    n_tracks = len(df.keys())
+
+    fig_width = 20
+    fig_depth = plot_depth * n_tracks
+
+    fig, axes = plt.subplots(nrows=n_tracks, ncols=3, figsize=(fig_width, fig_depth))
 
     position = 0
     for key in df.keys():
-        df[key].plot(x='X-location.1', y='Y-location.1', ax=axes[position, 0])
-        df[key].plot(y='Distance', ax=axes[position, 1], ylim=dist_ylim)
-        df[key].plot(y='Abs Angle', ax=axes[position, 2], ylim=angle_ylim)
-        position = position + 1
+        if n_tracks < 2:
+            ax = df[key].plot(x='X-location.1', y='Y-location.1', ax=axes[0])
+            ax.legend([key])
+            df[key].plot(y='Distance', ax=axes[1], ylim=dist_ylim)
+            df[key].plot(y='Abs Angle', ax=axes[2], ylim=angle_ylim)
+        else:
+            ax = df[key].plot(x='X-location.1', y='Y-location.1', ax=axes[position, 0])
+            ax.legend([key])
+            df[key].plot(y='Distance', ax=axes[position, 1], ylim=dist_ylim)
+            df[key].plot(y='Abs Angle', ax=axes[position, 2], ylim=angle_ylim)
+            position = position + 1
 
+
+def max_distance(df):
+    ''' Calculate the max distance moved across all tracks in a set'''
+    max_dist = 0
+
+    for key in df.keys():
+        md = df[key]['Distance'].max()
+        if md > max_dist:
+            max_dist = md
+
+    return max_dist
