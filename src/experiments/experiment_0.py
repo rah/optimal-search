c65b7f64e042724dd99d8074f268899c2618e97a
rah <rossh00@gmail.com>
2019-12-31 12:57:59 +1000

    experiments

--- src/experiments/experiment_0.py
+++ src/experiments/experiment_0.py
@@ -1,8 +1,21 @@
 #! /usr/env/python
 """
-Run simulation with default parameters
+Run simulation experiments
 """
 import src.experiments.runsim as rs
 
-er, cr = rs.runsim()
-rs.analyse_results(er, cr)
+def run_experiment(config=None):
+    er, cr = rs.runsim()
+    rs.analyse_results(er, cr)
+
+def experiment_0():
+    """
+    Run experiment with default internal params
+    """
+    run_experiment()
+
+def experiment_1():
+    """
+    Run experiment with default external params
+    """
+    run_experiment("default.ini")
