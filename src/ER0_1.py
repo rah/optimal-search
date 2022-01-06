10b8e46c6169e7d136d9ec5596bb802173ca28b5
rah <rossh00@gmail.com>
2022-01-06 12:06:53 +1000

    Update config path

--- src/ER0_1.py
+++ src/ER0_1.py
@@ -4,9 +4,9 @@ Random search
 """
 
 import simutil as su
-import analysis.analysis as an
+import src.analysis.analysis as an
 
-p = su.get_params("./src/experiments/config/ER0_1.ini")
+p = su.get_params("./experiments/config/ER0_1.ini")
 results = su.runsim(p)
 data = an.extract_data(results)
 an.analyse_results(data)
