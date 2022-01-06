9f0e74e6e06f0ead4139075fe6f13d05115a9b21
rah <rossh00@gmail.com>
2022-01-06 12:28:52 +1000

    Reconfigure config files

--- src/ER0_1.py
+++ src/ER0_1.py
@@ -4,9 +4,9 @@ Random search
 """
 
 import simutil as su
-import src.analysis.analysis as an
+import analysis.analysis as an
 
-p = su.get_params("./experiments/config/ER0_1.ini")
+p = su.get_params("./experiments/ER0_1.ini")
 results = su.runsim(p)
 data = an.extract_data(results)
 an.analyse_results(data)
