888cdb8cc9cc6b0f53af2f94e5135e4d7391b476
rah <rossh00@gmail.com>
2023-08-06 17:01:57 +1000

    display search path

---
+++ src/analysis/search_dist.py
@@ -0,0 +1,41 @@
+# Present the search area as a 2 dimentional rectange
+from scipy import stats
+import numpy as np
+import pylab
+
+def extract_search_paths(results):
+    """
+    Extracts the x and y coordinates from the given results.
+
+    Parameters:
+    - results (dict): A dictionary containing the results of the trials.
+
+    Returns:
+    - x (list): A list of x coordinates.
+    - y (list): A list of y coordinates.
+    """
+    # Set up arrays to sum the x and y coordinates for all trials
+    x = []
+    y = []
+
+    pred = results['predator']
+
+    # Add the steps for each predator from each trial
+    for trial in range(len(pred)):
+        for step in range(len(pred[trial].X)):
+            x.append(pred[trial].X[step])
+            y.append(pred[trial].Y[step])
+
+    return x, y
+
+def plot_search_dist(x, y):
+    """
+    Plot the search distribution.
+    
+    Parameters:
+        x (list): The x-coordinates of the points to plot.
+        y (list): The y-coordinates of the points to plot.
+    """
+    pylab.plot(x, y)
+    pylab.show()
+
---
+++ src/experiments/ER0_0.py
@@ -0,0 +1,22 @@
+#! /usr/env/python
+"""
+Random search
+"""
+# %%
+import os
+import sys
+sys.path.append('../../src')
+
+# %%
+import util.simutil as su
+import analysis.search_dist as sd
+
+#%%
+p = su.get_params("./ER0_0.ini")
+results = su.runsim(p)
+
+#%%
+x, y = sd.extract_search_paths(results)
+
+# %%
+sd.plot_search_dist(x, y)
